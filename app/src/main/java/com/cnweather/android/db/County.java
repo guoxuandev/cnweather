a3043a4e1484c08ccdf461d241e95e4d430f749b
guoxuandev <1220757299@qq.com>
2017-11-06 23:25:16 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/cnweather/android/db/County.java
@@ -0,0 +1,50 @@
+package com.cnweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/11/6 0006.
+ */
+
+public class County extends DataSupport{
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int CityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return CityId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        CityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/cnweather/android/db/Province.java
@@ -0,0 +1,41 @@
+package com.cnweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/11/5 0005.
+ */
+
+public class Province extends DataSupport{
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+
+    }
+}
