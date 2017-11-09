be21c72f62b269c12c0d0c2ccf80362074f5b72a
guoxuandev <1220757299@qq.com>
2017-11-09 21:17:24 +0800

    加入显示天气信息功能

--- app/src/main/java/com/cnweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/cnweather/android/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.cnweather.android;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -100,6 +101,19 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel==LEVEL_CITY){
                     selectedCity=cityList.get(position);
                     queryCounties();
+                }else if (currentLevel==LEVEL_COUNTY){
+                    String weatherId=countyList.get(position).getWeatherId();
+                    if (getActivity()instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if (getActivity()instanceof WeatherActivity){
+                        WeatherActivity activity=(WeatherActivity)getActivity();
+                        activity.drawerLayout.closeDrawers();
+                       // activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
---
+++ app/src/main/java/com/cnweather/android/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.cnweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/11/8 0008.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
